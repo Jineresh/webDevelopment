fb0b2fbd0e7ce2f707b9646924b917c4bc3f1c5e
Jineresh <32606284+Jineresh@users.noreply.github.com>
2019-04-04 22:29:14 +0800

    Add files via upload

---
+++ pomodoro/time.js
@@ -0,0 +1,11 @@
+var seconds = 100,
+    coountDiv = document.getElementById(çountdown),
+    secondPass,
+    countDown = setInterval(function () {
+        "use strict";
+        secondPass();
+    }, 1000);
+function secondPass() {
+    "use strict";
+    var minutes = Math.floor((seconds / 60)),
+}
