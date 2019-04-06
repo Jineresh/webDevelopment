2f6549158c8c0239d15733aac3dfb65c0b926b22
Jineresh <32606284+Jineresh@users.noreply.github.com>
2019-04-06 17:29:38 +0800

    Add files via upload

--- pomodoro/time.js
+++ pomodoro/time.js
@@ -1,22 +1,28 @@
-var seconds = 1500,
+function countDownStart() {
+    var seconds = 1500,
     countDiv = document.getElementById('countDown'),
     secondPass,
     countDown = setInterval(function () {
         "use strict";
         secondPass();
     }, 1000);
-function secondPass() {
-    "use strict";
-    var minutes = Math.floor((seconds / 60)),
-        remSeconds = seconds % 60;
-    if (seconds < 10) {
-        remSeconds = "0" + remSeconds;
+    function secondPass() {
+        "use strict";
+        var minutes = Math.floor((seconds / 60)),
+            remSeconds = seconds % 60;
+        if (seconds < 10) {
+            remSeconds = "0" + remSeconds;
+        }
+        countDiv.innerHTML = minutes + ":" + remSeconds;
+        if (seconds > 0) {
+            seconds = seconds - 1;
+        } else {
+            clearInterval(countDown);
+            countDiv.innerHTML = 'Done';
+        }
     }
-    countDiv.innerHTML = minutes + ":" + remSeconds;
-    if (seconds > 0) {
-        seconds = seconds - 1;
-    } else {
-        clearInterval(countDown);
-        countDiv.innerHTML = 'Done';
-    }
+}
+
+function countDownStop() {
+    
 }
